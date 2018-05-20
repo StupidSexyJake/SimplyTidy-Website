a512bcab92a76986907120ce1c51f0523e1518f4
Jake <jake@goldcoastmaids.com.au>
2018-05-20 21:31:54 +1000

    feat: Add booking form popup menu to homepage

--- js/script.js
+++ js/script.js
@@ -1,12 +1,75 @@
-function openSidemenu() {            
-    document.getElementById("sidebarMenu").classList.remove("hideSidebar");    
-    document.getElementById("sidebarMenu").classList.add("showSidebar");
-    document.getElementById("sidebar").classList.remove("makeHidden");    
-    document.getElementById("sidebar").classList.add("makeVisible");
-}
-function closeSidemenu() {        
-    document.getElementById("sidebarMenu").classList.remove("showSidebar");
-    document.getElementById("sidebarMenu").classList.add("hideSidebar");
-    document.getElementById("sidebar").classList.remove("makeVisible");
-    document.getElementById("sidebar").classList.add("makeHidden");
-}
+// VARIABLES
+    // Side navigation menu
+        var sidebar = document.getElementById("sidebar");        
+        var sidebarMenu = document.getElementById("sidebar-menu");
+        var sidemenuButton = document.getElementById("sidemenu-button");    
+        var closeSidemenuButton = document.getElementById("close-sidemenu");
+    // Booking form popup
+        var bookingFormPopup = document.getElementById("booking-form-popup");        
+        var bookingFormPopupForm = document.getElementById("booking-form-popup-form");
+        var bookingFormPopupButton = document.getElementById("homepage-call-to-action");
+        var closebookingFormPopupButton = document.getElementById("close-booking-form-popup");
+
+// FUNCTIONS
+    // Side navigation menu
+        // Overlay the side navigation menu over the main content area
+            function openSidemenu() {            
+                document.body.style.setProperty("--sidemenu-opacity", 1);
+                sidebar.style.pointerEvents = "auto";
+                sidebarMenu.classList.remove("transitionOutRightToLeft");    
+                sidebarMenu.classList.add("transitionInLeftToRight");
+            }
+        // Close the side navigation menu
+            function closeSidemenu() {
+                document.body.style.setProperty("--sidemenu-opacity", 0);
+                sidebar.style.pointerEvents = "none";
+                sidebarMenu.classList.remove("transitionInLeftToRight");
+                sidebarMenu.classList.add("transitionOutRightToLeft");
+            }
+    // Booking form popup
+        // Overlay the booking form popup over the main content area
+            function openBookingFormPopup() {            
+                document.body.style.setProperty("--booking-form-popup-opacity", 1);
+                bookingFormPopup.style.pointerEvents = "auto";
+                bookingFormPopupForm.classList.remove("transitionOutBottomToTop");    
+                bookingFormPopupForm.classList.add("transitionInTopToBottom");
+            }
+        // Close the booking form popup
+            function closebookingFormPopup() {
+                document.body.style.setProperty("--booking-form-popup-opacity", 0);
+                bookingFormPopup.style.pointerEvents = "none";
+                bookingFormPopupForm.classList.remove("transitionInTopToBottom");
+                bookingFormPopupForm.classList.add("transitionOutBottomToTop");
+            }
+
+// EVENT LISTENERS
+    // Navigation bar - side menu button        
+        sidemenuButton.addEventListener("click", function(){
+            // Open side menu
+            openSidemenu();
+            // Create event listener for side menu close button
+            closeSidemenuButton.addEventListener("click", function(){
+                closeSidemenu()
+            }); 
+            // Close side menu when user clicks outside menu
+            sidebar.addEventListener("click", function () {
+                if (!sidebarMenu.contains(event.target)) {
+                    closeSidemenu();
+                }
+            });
+        });
+    // Home page - booking form popup button     
+        bookingFormPopupButton.addEventListener("click", function(){
+            // Open side menu
+            openBookingFormPopup();
+            // Create event listener for side menu close button
+            closebookingFormPopupButton.addEventListener("click", function(){
+                closebookingFormPopup();
+            }); 
+            // Close side menu when user clicks outside menu
+            bookingFormPopup.addEventListener("click", function () {
+                if (!bookingFormPopupForm.contains(event.target)) {
+                    closebookingFormPopup();
+                }
+            });
+        });
---
+++ js/vendors/zoho-salesiq-chat-widget.js
@@ -0,0 +1,8 @@
+/**
+ * Source: https://salesiq.zoho.com/gcmaids/index#setting/embedchats
+ * Category: Online chat
+ * Purpose: Zoho SalesIQ chat plugin
+ * Instructions: Import script in footer of all client-facing pages
+*/
+
+var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode:"cc3595bcf2b1b5c15c83b5380d8f9499f5a6894966e58a35e59da06c597f5fe9", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
